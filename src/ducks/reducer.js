f17b5cacfbe9d7bff486a463434618da9a48ffca
Josiah Maina <josiah5115@gmail.com>
2018-09-03 17:25:52 -0600

    reducer

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -1,5 +1,30 @@
-function reducer(){
 
+const initialState = {
+ loanType: 'Home Purchase',
+ propertyType: 'Single Family Home',
+ city: '',
+ propToBeUsedOn: '',
+ found: "false",
+ realEstateAgent: "false",
+ cost: 0,
+ downPayment: 0,
+ credit: '',
+ history: '',
+ addressOne: '',
+ addressTwo: '',
+ addressThree: '',
+ firstName: '',
+ lastName: '',
+ email: '',
+}
+
+
+export default function reducer(state = initialState, action){
+ switch(action.type){
+  // case
+ }
+
+ return state;
 }
 
 export default reducer;
