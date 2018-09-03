96ef4fb3babc1613d21a4f6e9600f04b59aaa9c8
Josiah Maina <josiah5115@gmail.com>
2018-09-03 16:38:37 -0600

    restructure app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { HashRouter } from 'react-router-dom';
-import { Provuder } from 'react-redux';
-
 import App from './App';
 import './index.css';
 
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.render(
- <Provider store={store} >
-   <HashRouter>
-    <App />
-   </HashRouter>
- </Provider>,
-document.getElementById( 'root' ));
-
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>    
+, document.getElementById('root'));
