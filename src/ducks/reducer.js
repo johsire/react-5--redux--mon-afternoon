2b37d24e3a3a5d16eb5770ed527dff97bf528be4
Josiah Maina <josiah5115@gmail.com>
2018-09-03 19:46:43 -0600

    All remaining Cases

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -25,18 +25,18 @@ const UPDATE_PROPERTY_TYPE = "UPDATE_PROPERTY_TYPE";
 const UPDATE_CITY = "UPDATE_CITY";
 const UPDATE_PROP = "UPDATE_PROP";
 const UPDATE_FOUND = "UPDATE_FOUND";
+const UPDATE_REAL_ESTATE_AGENT = "UPDATE_REAL_ESTATE_AGENT";
 
-const REAL_ESTATE_AGENT = "UPDATE_REAL_ESTATE_AGENT";
-const COST = "UPDATE_COST";
-const DOWN_PAYMENT = "UPDATE_DOWN_PAYMENT";
-const CREDIT = "UPDATE_CREDIT";
-const HISTORY = "UPDATE_HISTORY";
-const ADDRESSONE = "UPDATE_ADDRESSONE";
-const ADDRESSTWO = "UPDATE_ADDRESSTWO";
-const ADDRESSTHREE = "UPDATE_ADDRESSTHREE";
-const FIRSTNAME = "UPDATE_FIRSTNAME";
-const LASTNAME = "UPDATE_LASTNAME";
-const EMAIL = "UPDATE_EMAIL";
+const UPDATE_COST = "UPDATE_COST";
+const UPDATE_DOWN_PAYMENT = "UPDATE_DOWN_PAYMENT";
+const UPDATE_CREDIT = "UPDATE_CREDIT";
+const UPDATE_HISTORY = "UPDATE_HISTORY";
+const UPDATE_ADDRESSONE = "UPDATE_ADDRESSONE";
+const UPDATE_ADDRESSTWO = "UPDATE_ADDRESSTWO";
+const UPDATE_ADDRESSTHREE = "UPDATE_ADDRESSTHREE";
+const UPDATE_FIRSTNAME = "UPDATE_FIRSTNAME";
+const UPDATE_LASTNAME = "UPDATE_LASTNAME";
+const UPDATE_EMAIL = "UPDATE_EMAIL";
 
 
 // ACTION CREATOR:
@@ -172,6 +172,39 @@ function reducer( state = initialState, action ) {
 
    case UPDATE_FOUND:
      return Object.assign( {}, state, { found: action.payload } );
+
+   case UPDATE_REAL_ESTATE_AGENT:
+     return Object.assign( {}, state, { realEstateAgent: action.payload } );
+
+     case UPDATE_COST:
+     return Object.assign( {}, state, { cost: action.payload } );
+
+     case UPDATE_DOWN_PAYMENT:
+     return Object.assign( {}, state, { downPayment: action.payload } );
+
+     case UPDATE_CREDIT:
+     return Object.assign( {}, state, { credit: action.payload } );
+
+     case UPDATE_HISTORY:
+     return Object.assign( {}, state, { history: action.payload } );
+
+     case UPDATE_ADDRESSONE:
+     return Object.assign( {}, state, { addressOne: action.payload } );
+
+     case UPDATE_ADDRESSTWO:
+     return Object.assign( {}, state, { addressTwo: action.payload } );
+
+     case UPDATE_ADDRESSTHREE:
+     return Object.assign( {}, state, { addressThree: action.payload } );
+
+     case UPDATE_FIRSTNAME:
+     return Object.assign( {}, state, { firstName: action.payload } );
+
+     case UPDATE_LASTNAME:
+     return Object.assign( {}, state, { lastName: action.payload } );
+
+     case UPDATE_EMAIL:
+     return Object.assign( {}, state, { email: action.payload } );
  }
 
  return state;
